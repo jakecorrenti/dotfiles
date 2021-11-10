94723951a49924d35a7064ccb7935fdbd8bb1643
Jake Correnti <jcorrenti13@outlook.com>
2021-11-09 21:44:35 -0500

    removed the commands that enabled italics in tmux. they really dont matter that much to me anymore

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,13 +6,6 @@ local vim = vim
 -- colorscheme
 vim.cmd[[colorscheme murphy]]
 
--- enables italics in neovim with tmux. need to see getpocket.com pin to 
--- create required files
-vim.cmd [[
-let &t_8f="\<Esc>[38;2;%lu;%lu;%lum"
-let &t_8b="\<Esc>[48;2;%lu;%lu;%lum"
-]]
-
 -- highlight the current yanked line
 vim.api.nvim_exec(
   [[
