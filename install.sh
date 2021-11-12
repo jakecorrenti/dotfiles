8a9ef32945d20ef8522d74417ca775f9d443c0cf
Jake Correnti <jcorrenti13@outlook.com>
2021-11-11 20:39:14 -0500

    moved git folder from dotfiles to .config

--- install.sh
+++ install.sh
@@ -127,6 +127,8 @@ ln -s ~/.config/.bashrc ~/.bashrc
 
 cd ~/
 
+mv ~/dotfiles/.git ~/.config/
+
 # github cli
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
