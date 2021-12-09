0b98580e3dfccf5d8d4b6772b4d6ae4387671416
Jake Correnti <jcorrenti13@outlook.com>
2021-12-09 11:23:33 -0500

    added install scripts again

---
+++ install.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+
+# things to install
+log_file=~/install_progress_log.txt
+
+# git
+sudo apt-get install git
+if type -p git &> /dev/null; then
+  echo "git installed" >> $log_file
+else
+  echo "git failed to install" >> $log_file
+fi
+
+# curl
+sudo apt-get install curl
+if type -p curl &> /dev/null; then
+  echo "curl installed" >> $log_file
+else
+  echo "curl failed to install" >> $log_file
+fi
+
+# cmake
+sudo apt-get install cmake
+if type -p cmake &> /dev/null; then
+  echo "cmake installed" >> $log_file
+else
+  echo "cmake failed to install" >> $log_file
+fi
+
+# python
+sudo apt-get install python3
+if type -p python3 &> /dev/null; then
+  echo "python3 installed" >> $log_file
+else
+  echo "python3 failed to install" >> $log_file
+fi
+
+# clangd
+sudo apt-get install clang
+if type -p clang &> /dev/null; then
+  echo "clang installed" >> $log_file
+else
+  echo "clang failed to install" >> $log_file
+fi
+
+# neovim
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt-get update
+sudo apt-get install neovim
+if type -p nvim &> /dev/null; then
+  echo "neovim installed" >> $log_file
+else
+  echo "neovim failed to install" >> $log_file
+fi
+
+# tmux
+sudo apt-get install tmux
+if type -p tmux &> /dev/null; then
+  echo "tmux installed" >> $log_file
+else 
+  echo "tmux failed to install" >> $log_file
+fi
+
+# awesome wm
+sudo apt-get install awesome
+
+# xrandr
+sudo apt-get install xrandr
+
+# autorandr
+sudo apt-get install arandr
+
+# rofi
+sudo apt-get install rofi
+
+# rust 
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+if type -p rustc &> /dev/null; then
+  echo "rust installed" >> $log_file
+else
+  echo "rust failed to install" >> $log_file
+fi
+
+# SourceCodePro NF Font
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/SourceCodePro.zip
+unzip SourceCodePro.zip -d ~/.fonts
+fc-cache -fv
+
+cd ~/
+
+# dotfiles
+git clone https://github.com/jakecorrenti/dotfiles.git
+
+cd dotfiles
+
+# neovim
+rm ~/.config/nvim
+mv nvim ~/.config/
+
+# .gitconfig
+rm ~/.gitconfig
+mv .gitconfig ~/.config/
+ln -s ~/.config/.gitconfig ~/.gitconfig
+
+# tmux
+rm ~/.tmux.conf
+mv .tmux.conf ~/.config/
+ln -s ~/.config/.tmux.conf ~/.tmux.conf
+
+# .bashrc
+rm ~/.bashrc
+mv .bashrc ~/.config/
+ln -s ~/.config/.bashrc ~/.bashrc
+
+cd ~/
+
+mv ~/dotfiles/.git ~/.config/
+
+# github cli
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install gh
+if type -p gh &> /dev/null; then
+  echo "github cli installed" >> $log_file
+else
+  echo "github cli failed to install" >> $log_file
+fi
+gh auth login
+
+echo "######### SUMMARY ########"
+cat $log_file
+rm $log_file
+
+echo "######## NOTE ########"
+echo "restart your terminal before doing rust install script"
---
+++ rust_install.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# things to install
+log_file=~/install_progress_log.txt
+
+
+# bat (cargo)
+cargo install bat
+if type -p bat &> /dev/null; then
+  echo "bat installed" >> $log_file
+else
+  echo "bat failed to install" >> $log_file
+fi
+
+# exa (cargo)
+cargo install exa
+if type -p exa &> /dev/null; then
+  echo "exa installed" >> $log_file
+else
+  echo "exa failed to install" >> $log_file
+fi
+
+# delta (cargo)
+cargo install git-delta
+if type -p delta &> /dev/null; then
+  echo "delta installed" >> $log_file
+else
+  echo "delta failed to install" >> $log_file
+fi
+
+# ripgrep (cargo)
+cargo install ripgrep
+if type -p rg &> /dev/null; then
+  echo "ripgrep installed" >> $log_file
+else
+  echo "ripgrep failed to install" >> $log_file
+fi
+
+# cargo-watch (cargo)
+cargo install cargo-watch
+if type -p cargo-watch &> /dev/null; then
+  echo "cargo-watch installed" >> $log_file
+else
+  echo "cargo-watch failed to install" >> $log_file
+fi
+
+# alacritty 
+git clone https://github.com/alacritty/alacritty.git
+cd alacritty
+
+rustup override set table
+rustup update stable
+
+sudo apt-get install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
+
+cargo build --release
+
+sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
+sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+sudo desktop-file-install extra/linux/Alacritty.desktop
+sudo update-desktop-database
+
+cd ~/
+
+cd ~/dotfiles
+
+rm ~/.config/alacritty
+mv alacritty ~/.config/
+
+cd ~/
+rm -rf ~/dotfiles
+
+echo "######### SUMMARY ########"
+cat $log_file
+rm $log_files
