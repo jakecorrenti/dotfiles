f1a03514f0bfa891f57adc59cfd394c841d9b5f8
Jake Correnti <jcorrenti13@outlook.com>
2022-01-19 20:26:33 -0500

    added nvim stuff back

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,75 +1,24 @@
 require("options")
 require("keymaps")
+require("plugins")
+require("telescope_conf")
+require("cmp_conf")
 
 local vim = vim
-local opt = vim.opt
-local g = vim.g
-local o = vim.o
-local key_mapper = function(mode, key, result)
-	vim.api.nvim_set_keymap(mode, key, result, { noremap = true, silent = true })
-end
 
+local default_colors = require'kanagawa.colors'.setup()
+require 'kanagawa'.setup ({
+    colors = {
+        diag = {
+            error = default_colors.autumnRed,
+            warning = default_colors.autumnYellow,
+            info = default_colors.dragonBlue,
+            hint = default_colors.waveAqua1,
+        }
+    },
+})
 
---
--- options
---
-
-g.mapleader = " "
-o.shortmess = vim.o.shortmess .. "c"
-
-opt.completeopt = "menu,menuone,noselect"
-opt.pumblend = 15
-opt.scrolloff = 4
-opt.laststatus = 1
-opt.showcmd = true
-opt.incsearch = true
-opt.expandtab = true
-opt.tabstop = 4
-opt.shiftwidth = 4
-opt.softtabstop = 4
-opt.smartcase = true
-opt.ignorecase = true
-opt.smartindent = true
-opt.updatetime = 50
-opt.hlsearch = true
-opt.termguicolors = true
-opt.expandtab = true
-opt.showmode = true
-opt.number = true
-opt.relativenumber = true
-opt.wrap = true
-opt.signcolumn = "no"
-
--- 
--- keymaps
---
-
-key_mapper("i", "jk", "<ESC>")
-
--- terminal navigation
-key_mapper("t", "<C-h>", "<C-\\><C-N><C-w>h")
-key_mapper("t", "<C-j>", "<C-\\><C-N><C-w>j")
-key_mapper("t", "<C-k>", "<C-\\><C-N><C-w>k")
-key_mapper("t", "<C-l>", "<C-\\><C-N><C-w>l")
-key_mapper("t", "jk", "<C-\\><C-n>")
-
--- window navigation
-key_mapper("n", "<C-j>", "<C-w>j")
-key_mapper("n", "<C-h>", "<C-w>h")
-key_mapper("n", "<C-k>", "<C-w>k")
-key_mapper("n", "<C-l>", "<C-w>l")
-key_mapper("n", "<leader>-", ":resize -10 <CR>")
-key_mapper("n", "<leader>+", ":resize +10 <CR>")
-key_mapper("n", "<leader>>", ":vertical resize +10 <CR>")
-key_mapper("n", "<leader><", ":vertical resize -10 <CR>")
-
--- window splitting
-key_mapper("n", "<leader>v", ":vsp <CR>")
-key_mapper("n", "<leader>s", ":sp <CR>")
-
--- 
--- execs
---
+require('onedarkpro').load()
 
 -- highlight the current yanked line
 vim.api.nvim_exec(
@@ -81,3 +30,18 @@ augroup END
 ]],
 	false
 )
+
+vim.api.nvim_exec([[
+autocmd BufWritePre * silent! lua vim.lsp.buf.formatting()
+]], false)
+
+-- treesitter
+local configs = require "nvim-treesitter.configs"
+configs.setup { ensure_installed = "maintained", highlight = { enable = true } }
+
+local lsp_installer = require("nvim-lsp-installer")
+
+lsp_installer.on_server_ready(function(server)
+    local opts = {}
+    server:setup(opts)
+end)
---
+++ nvim/lua/cmp_conf.lua
@@ -0,0 +1,27 @@
+local cmp = require "cmp"
+
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      -- For `luasnip` user.
+      require("luasnip").lsp_expand(args.body)
+    end,
+  },
+  mapping = {
+    ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-d>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.close(),
+    ["<CR>"] = cmp.mapping.confirm { select = true },
+  },
+
+  sources = {
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+    { name = "buffer" },
+    { name = "path" },
+  },
+  documentation = {
+    border = "single",
+  },
+}
