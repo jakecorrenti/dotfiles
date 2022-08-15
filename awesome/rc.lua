8109af49d762a5ced0eef555972efe4320dbae8c
Jake Correnti <jcorrenti13@gmail.com>
2022-08-15 18:53:15 -0400

    awesome misc

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -571,5 +571,5 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 
 awful.spawn.with_shell("~/.config/resolution_change.sh")
 awful.spawn.with_shell("feh --bg-fill /home/jakecorrenti/Pictures/forest-hut.png")
-beautiful.useless_gap = 4
+beautiful.useless_gap = 5
 beautiful.gap_single_client = true
